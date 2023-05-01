3306833bb94104384ef0f4134d808f6e246fe9bc
Mannnny <38955706+mannny7@users.noreply.github.com>
2023-05-01 16:32:37 +0100

    Add files via upload

---
+++ mtools/ReturnMessage.java
@@ -0,0 +1,40 @@
+package com.manny.mtools;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static com.manny.mtools.MTools.pluginName;
+
+public class ReturnMessage {
+
+    // Use to return a message to a player or to the console
+    public static void send(CommandSender sender, String message) {
+
+        if (sender instanceof Player) {
+            sender.sendMessage(ChatColor.GREEN + pluginName + " >>> " + message);
+        } else {
+            ConsoleLog.log(message);
+        }
+
+    }
+
+
+    // Use to log error in commands to the console or player
+    public static void error(CommandSender sender, String message) {
+        if (sender instanceof Player) {
+            sender.sendMessage(ChatColor.RED + pluginName + " >>> " + message);
+        } else {
+            ConsoleLog.error(message);
+        }
+    }
+
+    // Use to inform the player or console that the incorrect number of arguments has been passsed
+
+    public static void argumentError(CommandSender sender, int desiredAmount) {
+        ReturnMessage.error(sender, String.format("This command takes exactly %d parameters", desiredAmount));
+
+    }
+
+
+}
---
+++ mtools/commands/db/freeze/IsFrozen.java
@@ -0,0 +1,50 @@
+package com.manny.mtools.commands.db.freeze;
+
+import com.manny.mtools.MTools;
+import com.manny.mtools.ReturnMessage;
+import com.manny.mtools.playerdb.CustomPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.sql.SQLException;
+
+public class IsFrozen implements CommandExecutor {
+
+    MTools main = (MTools) Bukkit.getPluginManager().getPlugin("MTools");
+    @Override
+    public boolean onCommand(CommandSender commandSender, org.bukkit.command.Command command, java.lang.String s, java.lang.String[] strings) {
+
+        if (strings.length != 1) {
+            ReturnMessage.argumentError(commandSender, 1);
+            return false;
+        }
+        // Find the player based of the IGN passed
+        Player recipient = Bukkit.getPlayerExact(strings[0]);
+
+        if (recipient == null) {
+            ReturnMessage.error(commandSender, "Invalid player");
+            return false;
+        }
+
+        try {
+            CustomPlayer player = new CustomPlayer(main, recipient.getUniqueId());
+
+            int currentFreeze = player.getFrozen();
+
+            // The database uses tinyints instead of booleans, so 0 is false and 1 is true
+
+            if (currentFreeze == 0) {
+                ReturnMessage.send(commandSender, strings[0] + " is not frozen");
+            } else {
+                ReturnMessage.send(commandSender, strings[0] + " is frozen");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
